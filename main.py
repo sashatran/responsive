94fa17354b767f113b24d9fa1c0175c6b9e4a741
Sasha Patel <Sasha@Sashas-MacBook-Air.local>
2017-01-19 22:48:54 -0800

    working layout

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+import webapp2
+import jinja2
+import random
+
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
+
+class BaseHandler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class MainPage(BaseHandler):
+    def get(self):
+        self.render("index.html")
+
+app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
